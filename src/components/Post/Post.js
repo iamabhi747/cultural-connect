c24255bec91da0309e947ede6413645dfbd05fd1
Abhinandan Bhuse <i.am.abhi747@gmail.com>
2024-03-31 11:26:01 +0530

    minor changes form previous commit

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -52,40 +52,4 @@ export default function Post() {
       </div>
     </div>
   ));
-
-  return (
-    <>
-      <div className="card m-auto mt-2 rounded-3xl" style={
-        {
-            width: '25rem'
-        }
-      }>
-    <div className="head flex w-[25rem] rounded-2xl ">
-      <div className='flex w-[23rem] m-3'>
-      <img src={img} alt=""className='w-[4em] h-[4rem] mr-auto' />
-      <div className="info flex-col mr-[10rem] ">
-        <p className='font-bold'>Heading</p>
-        <p className='text-sm text-nowrap'>Added to Timeline</p>
-        <p className='text-sm'>16/10/2005</p>
-      </div>
-      <img src={dot} alt="" className='bg-cover w-[2rem] h-[2rem] mt-3 ml-auto' />
-      </div>
-    </div>
-    <hr />
-    <div className='title bg-white text-xl font-bold ml-[1rem] text-left'>
-      Spectacular UNESCO Heritage Sites In Maharashtra
-    </div>
-  <img src={mountain} className="card-img p-3" alt=''></img>
-  <div className="card-body">
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <div className='more flex space-x-5 h-10 mt-4'>
-    <img src={like} alt="" className='h-8 coursor-pointer ml-2' />
-    <img src={chat} alt="" className='h-8 ml-2' />
-    <img src={share} alt="" className='h-8'/>
-    </div>
-  </div>
-</div>
-      
-    </>
-  )
 }
