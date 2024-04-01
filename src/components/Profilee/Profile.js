f97b13abc47d3038ddb7710ea6cbcb8b92d359be
Atharva Kavade <atharvadk27@gmail.com>
2024-04-01 11:10:56 +0530

    profile page data updated

--- src/components/Profilee/Profile.js
+++ src/components/Profilee/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Profile from './Profile.jpg';
 import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn, MDBTypography } from 'mdb-react-ui-kit';
 
 export default function EditButton() {
@@ -10,23 +11,23 @@ export default function EditButton() {
             <div>
               <div className="rounded-top text-white d-flex flex-row" style={{ backgroundColor: '#000', height: '200px' }}>
                 <div className="ms-4 mt-5 d-flex flex-column" style={{ width: '150px' }}>
-                  <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                  <MDBCardImage src={Profile}
                     alt="Generic placeholder image" className="mt-4 mb-2 img-thumbnail" fluid style={{ width: '150px', zIndex: '1' }} />
-                  
+
                 </div>
                 <div className="ms-3" style={{ marginTop: '130px' }}>
-                  <MDBTypography tag="h5">Andy Horwitz</MDBTypography>
-                  <MDBCardText>New York</MDBCardText>
+                  <MDBTypography tag="h5">Abhinandan Bhuse</MDBTypography>
+                  <MDBCardText>Pune</MDBCardText>
                 </div>
               </div>
               <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa' }}>
                 <div className="d-flex justify-content-end text-center py-1">
-                    <button className='mr-12 curosor-pointer border-1 rounded-md p-2 border-black'>
+                  <button className='mr-12 curosor-pointer border-1 rounded-md p-2 border-black'>
                     Edit profile
                   </button>
                   <div>
-                    <MDBCardText className="mb-1 h5">253</MDBCardText>
-                    <MDBCardText className="small text-muted mb-0">Photos</MDBCardText>
+                    <MDBCardText className="mb-1 h5">4</MDBCardText>
+                    <MDBCardText className="small text-muted mb-0">Posts</MDBCardText>
                   </div>
                   <div className="px-3">
                     <MDBCardText className="mb-1 h5">1026</MDBCardText>
@@ -42,9 +43,10 @@ export default function EditButton() {
                 <div className="mb-5">
                   <p className="lead fw-normal mb-1">About</p>
                   <div className="p-4" style={{ backgroundColor: '#f8f9fa' }}>
-                    <MDBCardText className="font-italic mb-1">Web Developer</MDBCardText>
-                    <MDBCardText className="font-italic mb-1">Lives in New York</MDBCardText>
-                    <MDBCardText className="font-italic mb-0">Photographer</MDBCardText>
+                    <MDBCardText className="font-italic mb-0">Heritage Explorer</MDBCardText>
+                    <MDBCardText className="font-italic mb-1">Backdend Developer</MDBCardText>
+                    <MDBCardText className="font-italic mb-1">Lives in Pune, Maharashtra</MDBCardText>
+                    <MDBCardText className="font-italic mb-0">Ethical Hacker</MDBCardText>
                   </div>
                 </div>
                 <div className="d-flex justify-content-between align-items-center mb-4">
@@ -53,17 +55,17 @@ export default function EditButton() {
                 </div>
                 <MDBRow>
                   <MDBCol className="mb-2">
-                    <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(112).webp"
+                    <MDBCardImage src="https://static.toiimg.com/thumb/msid-105827889,imgsize-1208261,width-400,resizemode-4/105827889.jpg"
                       alt="image 1" className="w-100 rounded-3" />
                   </MDBCol>
                   <MDBCol className="mb-2">
-                    <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(107).webp"
+                    <MDBCardImage src="https://static.toiimg.com/thumb/msid-108073571,imgsize-138876,width-400,resizemode-4/108073571.jpg"
                       alt="image 1" className="w-100 rounded-3" />
                   </MDBCol>
                 </MDBRow>
                 <MDBRow className="g-2">
                   <MDBCol className="mb-2">
-                    <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(108).webp"
+                    <MDBCardImage src="https://static.toiimg.com/thumb/msid-107897936,imgsize-62288,width-400,height-225,resizemode-72/107897936.jpg"
                       alt="image 1" className="w-100 rounded-3" />
                   </MDBCol>
                   <MDBCol className="mb-2">
