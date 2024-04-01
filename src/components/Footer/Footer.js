57784e4a1b86eb799b2f0533975c62aa35e5495d
Atharva Kavade <atharvadk27@gmail.com>
2024-04-01 11:27:37 +0530

    footer updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,9 +7,9 @@ import Profile from './Profile.jpeg'
 
 
 import {
-    Link,
-    useLocation
-  } from "react-router-dom";
+  Link,
+  useLocation
+} from "react-router-dom";
 
 
 
@@ -23,56 +23,55 @@ function Footer() {
     console.log(url)
   }, [location]);
 
-  
-  
+
+
   return (
-    
+
     <>
-     <nav className={`navbar ${url === '/' ? 'sticky' : 'fixed'} bottom-0 bg-[rgba(253,247,235,0.8)]  w-[100%] rounded-t-3xl  `} style={
-      {
-        boxShadow: '0.5px -0.5px rgba(0, 0, 0, 0.8)'
-      }
-     }>
-  <div className="container-fluid ">
-        <div className="home ml-5 ">
+      <nav className={`navbar ${url === '/' ? 'sticky' : 'fixed'} bottom-0 bg-[rgba(253,247,235,0.8)]  w-[100%] rounded-t-3xl  `} style={
+        {
+          boxShadow: '0.5px -0.5px rgba(0, 0, 0, 0.8)'
+        }
+      }>
+        <div className="container-fluid ">
+          <div className="home ml-5 ">
             <Link to="/">
-            <img src={home} alt="" className={`h-[40px] w-[40px] sticky border-1 rounded-full p-1  border-black ${
-              url === '/' ? 'bg-gradient-to-b from-[#0b97fb] to-[#3bf774]' : 'bg-white' 
-              } `}>
-            </img>
-            <p className='font-bold text-sm'>Home</p>
+              <img src={home} alt="" className={`h-[40px] w-[40px] sticky border-1 rounded-full p-1  border-black ${url === '/' ? 'bg-gradient-to-b from-[#0b97fb] to-[#3bf774]' : 'bg-white'
+                } `}>
+              </img>
+              <p className='font-normal text-sm'>Home</p>
             </Link>
-        </div>
-        <div className="create text-sm ">
-        <Link to="/create">
-            <img src={create} alt="" className={`h-[38px] w-[38px] border-1  rounded-full p-[2px] border-black ${url === '/create' ? 'bg-gradient-to-b from-[#0b97fb] to-[#3bf774]'  : 'bg-white' } `}>
-            </img>
-            <p className='font-bold text-sm'>Create</p>
-        </Link>
-        </div>
-        <div className="timeline text-sm ">
-        <Link to="/timeline">
-        <img src={Timeline} alt="" className={` h-[40px] w-[40px] ml-3 border-1 rounded-full p-1 border-black ${url === '/timeline' ? 'bg-gradient-to-b from-[#0b97fb] to-[#3bf774]' : 'bg-white' } `}>
-            </img>
-            <p className='font-bold'>Timeline</p>
-        </Link>
-        </div>
-        <div className="community text-sm ">
-        <Link to="/community">
-        <img src={Community} alt="" className={`h-[40px] w-[40px] ml-5 border-1 rounded-full p-1 border-black ${url === '/community' ? 'bg-gradient-to-b from-[#0b97fb] to-[#3bf774]' : 'bg-white' } `}>
-            </img>
-            <p className='font-bold'>Community</p>
+          </div>
+          <div className="create text-sm ">
+            <Link to="/create">
+              <img src={create} alt="" className={`h-[38px] w-[38px] border-1  rounded-full p-[2px] border-black ${url === '/create' ? 'bg-gradient-to-b from-[#0b97fb] to-[#3bf774]' : 'bg-white'} `}>
+              </img>
+              <p className='font-normal text-sm'>Create</p>
             </Link>
+          </div>
+          <div className="timeline text-sm ">
+            <Link to="/timeline">
+              <img src={Timeline} alt="" className={` h-[40px] w-[40px] ml-3 border-1 rounded-full p-1 border-black ${url === '/timeline' ? 'bg-gradient-to-b from-[#0b97fb] to-[#3bf774]' : 'bg-white'} `}>
+              </img>
+              <p className='font-normal'>Timeline</p>
+            </Link>
+          </div>
+          <div className="community text-sm ">
+            <Link to="/community">
+              <img src={Community} alt="" className={`h-[40px] w-[40px] ml-5 border-1 rounded-full p-1 border-black ${url === '/community' ? 'bg-gradient-to-b from-[#0b97fb] to-[#3bf774]' : 'bg-white'} `}>
+              </img>
+              <p className='font-normal'>Community</p>
+            </Link>
+          </div>
+          <div className="profile text-sm ">
+            <Link to="/profile">
+              <img src={Profile} alt="" className={`h-[40px] w-[40px] mr-4 border-1 rounded-full p-1 border-black ${url === '/profile' ? 'bg-gradient-to-b from-[#0b97fb] to-[#3bf774]' : 'bg-white'}  `}>
+              </img>
+              <p className='font-normal'>Profile</p>
+            </Link>
+          </div>
         </div>
-        <div className="profile text-sm ">
-        <Link to="/profile">
-        <img src={Profile} alt="" className={`h-[40px] w-[40px] mr-4 border-1 rounded-full p-1 border-black ${url === '/profile' ? 'bg-gradient-to-b from-[#0b97fb] to-[#3bf774]' : 'bg-white' }  `}>
-            </img>
-            <p className='font-bold'>Profile</p>
-        </Link>
-        </div>
-    </div>
-    </nav> 
+      </nav>
     </>
   )
 }
